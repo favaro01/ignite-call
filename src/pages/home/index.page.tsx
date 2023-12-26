956822b12f00ccfdf59bc6ca25d2173e3774a3c0
Rafael Favaro Dias <favarote01@hotmail.com>
2023-12-26 15:35:18 -0300

    first commit

---
+++ src/pages/home/index.page.tsx
@@ -0,0 +1,29 @@
+import { Heading, Text } from "@ignite-ui/react"
+import { Container, Hero, Preview } from "./styles"
+import Image from "next/image"
+
+import PreviewImage from "../../assets/app.png";
+import { ClaimUserNameForm } from "./components/ClaimUserNameForm";
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.
+        </Text>
+        <ClaimUserNameForm />
+      </Hero>
+      <Preview>
+        <Image 
+          src={PreviewImage} 
+          height={400}
+          quality={100}
+          priority
+          alt="Calendario simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
